c6ee137cf6922e691183fc62829a2971c23850a8
Jonathan Quemuel <jkq@hawaii.edu>
2016-10-09 22:18:20 -1000

    Task 5: subscriptions
    removed autopublish and implemented private access

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,21 +1,23 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
-    tasks: function () {
-      return Tasks.find({}, { sort: { createdAt: -1 } });
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.tasks.events({
-    "submit .add-task": function (event) {
-      var name = event.target.name.value;
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
       Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
 
-    "click .delete-task": function (event) {
+    "click .delete-task": function(event) {
       if (confirm('Delete Task?')) {
         Meteor.call('deleteTask', this._id);
       }
@@ -25,11 +27,13 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
 
 Meteor.methods({
-  addTask: function (name) {
+  addTask: function(name){
     if (!Meteor.userId()) {
       throw new Meteor.Error('No Access');
     }
@@ -38,7 +42,7 @@ Meteor.methods({
     });
   },
 
-  deleteTask: function (taskId) {
+  deleteTask: function(taskId) {
     Tasks.remove(taskId);
   }
 })
