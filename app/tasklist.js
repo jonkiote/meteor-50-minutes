900f711be90c678037ba9f3968ff15d979acc91b
Jonathan Quemuel <jkq@hawaii.edu>
2016-10-09 22:07:21 -1000

    Task 4: Tasklist authentication
    Added user log in

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,37 +1,45 @@
-/**
- * Created by Jonkiote on 10/9/2016.
- * Jonathan Quemuel
- */
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function () {
+      return Tasks.find({}, { sort: { createdAt: -1 } });
     }
   });
 
   Template.tasks.events({
-    "submit .add-task": function(event){
-      let name = event.target.name.value;
-      //console.log(name);
-
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
-
+    "submit .add-task": function (event) {
+      var name = event.target.name.value;
+      Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
-    "click .delete-task": function(event){
-      if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+
+    "click .delete-task": function (event) {
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
       }
+      return false;
     }
   })
 }
 
-if (Meteor.isServer){
+if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function (name) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  }
+})
+
